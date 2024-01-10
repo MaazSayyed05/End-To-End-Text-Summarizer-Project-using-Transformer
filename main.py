593ad7addfd6568b967e6c8620456c14dbba1f1e
MaazSayyed05 <maazsayyed05@gmail.com>
2024-01-10 21:56:25 +0530

    data validation notebook added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from Text_Summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Text_Summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 from Text_Summarizer.logging import logger
 
@@ -8,6 +9,19 @@ try:
     logger.info(f">>>>>>>>>>>>> Initialize {STAGE_NAME}  <<<<<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>>>>>>>> Completed {STAGE_NAME} Successfully  <<<<<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME =  "Data Validaiton Stage"
+try:
+    logger.info(f">>>>>>>>>>>>> Initialize {STAGE_NAME}  <<<<<<<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>>>>>>>>> Completed {STAGE_NAME} Successfully  <<<<<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
--- src/Text_Summarizer/logging/__init__.py
+++ src/Text_Summarizer/logging/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
 
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]" 
